69c8012ce7bd06ae01f88f6591e3a69012bc45c3
Joshtg1104 <joshtg1104@gmail.com>
2019-06-13 15:24:24 -0500

    Finally solved my biggest problem

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 
@@ -6,24 +6,26 @@ class SignIn extends Component {
 
     constructor(props) {
         super(props);
-        this.state={
-            data:[],
-            image:"",
-            posts: [],
+        this.state = {
+            data: [],
+            image: "",
+            posts: null,
         };
     }
+
     //mounts displayPosts function
     componentDidMount() {
         this.displayPosts();
     }
+
     //event handler that that fetches user/login route to allow user to login
-    submitLogin=(e)=>{
+    submitLogin = (e) => {
         e.preventDefault();
         console.log("Loggingg In... Logged In");
         fetch('/users/login',
             {
                 method: 'POST',
-                headers:{
+                headers: {
                     "Accept": "application/json",
                     "Content-Type": "application/json",
                 },
@@ -33,46 +35,55 @@ class SignIn extends Component {
                     // image: e.target.image.value
                 }),
             })
-            .then(data=>data.json())
-            .then(data=>{
+            .then(data => data.json())
+            .then(data => {
                 console.log("Here is data");
                 console.log(data);
-                if(data)
+                if (data)
                     return this.props.userLoggedIn(data, true);
                 else
                     return this.props.userLoggedIn(data, false)
             });
     };
     //fetches postlist from user route which displays all post made on site
-    displayPosts=()=>{
+    displayPosts = () => {
         fetch('/users/postList')
             .then(posts => posts.json())
-            .then(postData=> this.setState({posts: postData}))
+            .then(postData => this.setState({posts: postData}))
     };
 
     render() {
         console.log(this.props.userLogInfo.signedIn + "work ");
         console.log(this.state.posts);
         //maps all post from user with index 0
+
         let postList = [];
-            if (this.state.posts[0]){
-                postList = this.state.posts[0].post.map(
-                    (post, index) => {
-                        return (
-                            <div key={index}>
-                                {/*<p>{post}</p>*/}
-                                <p>{this.state.posts[0].username}</p>
-                                <p>{post}</p>
-                                <hr/>
-                            </div>
-                        )
-                    }
-                );
+        let user = [];
+        if (this.state.posts) {
+            console.log("user");
+            console.log(this.state.posts);
+            postList = this.state.posts.map((user) => {
+                    // console.log("posts");
+                    // console.log(posts);
+                    return user.post.map((tweet) => {
+                            // console.log("tweet");
+                            // console.log(tweet);
+                            return (
+                                <div>
+                                    <p><strong>{user.username}</strong></p>
+                                    <p>{tweet}</p>
+                                    <hr/>
+                                </div>
+                            )
+                        }
+                    )
+                }
+            );
         }
 
-        if(this.props.userLogInfo.signedIn){
+        if (this.props.userLogInfo.signedIn) {
             console.log(this.props + "A");
-            return(
+            return (
                 <div>
                     {/*<img src="LeeMan" alt="" */}
                     {/*{this.props.userLogInfo.image}*/}
@@ -81,8 +92,7 @@ class SignIn extends Component {
                     {postList}
                 </div>
             );
-        }
-        else{
+        } else {
             return (
                 <div className="App">
                     <h1>Sign-In</h1>
