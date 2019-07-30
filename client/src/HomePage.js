16106f931efbb9c189ec463c4711a6129732c221
Joshtg1104 <joshtg1104@gmail.com>
2019-07-30 14:51:39 -0500

    Ok

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -102,10 +102,6 @@ class HomePage extends Component {
                         <Link to='/userprofile'>Profile</Link>
                         <Link to='/signup'>Sign-Up</Link>
                         {/*<Link to='/signin'>Log In</Link>*/}
-                        <form>
-                            <input type="text" placeholder={"Search"}/>
-                            <button>Submit</button>
-                        </form>
 
                         {/*routes connect links to components*/}
                         <Route exact path='/' component={()=>{return <SignIn userLogInfo={this.state.userLogInfo} userLoggedIn={this.userLoggedIn}/>}}/>
@@ -128,10 +124,12 @@ class HomePage extends Component {
                         <Link to='/'>Home</Link>
                         <Link to='/userprofile'>Profile</Link>
                         <Link to='/logout' onClick={this.userLogOut}>Log Out</Link>
+                        <br/>
                         <form>
                             <input type="text" placeholder={"Search"}/>
                             <button>Submit</button>
                         </form>
+                        <br/>
 
                         <Route exact path='/' component={()=>{return <SignIn userLogInfo={this.state.userLogInfo} userLoggedIn={this.userLoggedIn}/>}}/>
                         <Route exact path='/userprofile' component={()=>{return <BlogTweets userLogInfo={this.state.userLogInfo} userLoggedIn={this.userLoggedIn}/>}}/>
